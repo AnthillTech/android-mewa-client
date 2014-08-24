2e8feaa6e88c4eb1addfa88fa086b28e7dee6995
Jacek <jdermont@sigma.ug.edu.pl>
2014-08-24 14:19:05 +0200

    updated example to 0.5 API

--- MewaClientExample/src/cc/mewa/androidclientexample/MainActivity.java
+++ MewaClientExample/src/cc/mewa/androidclientexample/MainActivity.java
@@ -14,27 +14,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import cc.mewa.MewaClient;
-import cc.mewa.MewaClient.AlreadyConnectedToChannelException;
-import cc.mewa.MewaClient.InitConnectionException;
+import cc.mewa.MewaConnection;
+import cc.mewa.MewaConnection.InitConnectionException;
 import cc.mewa.OnMessageListener;
 
 public class MainActivity extends Activity implements Handler.Callback {
 	public static final String TAG = "MainActivity";
 	
 	public static final String WS_URI = "ws://mewa.cc/ws";
-	public static final String CHANNEL = "test.channel1";
+	public static final String CHANNEL = "jdermont.channel1";
 	public static final String DEVICE = "android";
-	public static final String PASSWORD = "test";
+	public static final String PASSWORD = "c1";
 	
 	Button connectBtn,deviceListBtn,sendEventBtn;
 	EditText loggerTxt;
 	
-	boolean connected;
 	Handler handler;
 	AsyncTask<String,String,String> task;
 	
-	MewaClient client;
+	MewaConnection connection;
 	OnMessageListener onMessageListener;
 
     @Override
@@ -45,28 +43,36 @@ public class MainActivity extends Activity implements Handler.Callback {
         connectBtn = (Button)findViewById(R.id.connectBtn);
         deviceListBtn = (Button)findViewById(R.id.deviceListBtn);
         sendEventBtn = (Button)findViewById(R.id.sendEventBtn);
-        buttonsDisable();
+        clientIsDisconnected();
         loggerTxt = (EditText)findViewById(R.id.loggerTxt);
         
-        connected = false;
         handler = new Handler(this);
         
         onMessageListener = new OnMessageListener() {
-        	
 			@Override
 			public void onConnected() {
+				Log.d(TAG,"onConnected()");
 				handler.sendEmptyMessage(0);
 			}
 
+			// note that this happens only when client explicitly gets disconnect signal from the channel
+			// the WebSocket itself might be still alive
 			@Override
 			public void onDisconnected() {
-				// note that this happens only when client explicitly gets disconnect signal from the channel
-				connected = false;
-				buttonsDisable();
+				Log.d(TAG,"onDisconnected()");
+			}
+			
+			// happens whenever WebSocket is closed
+			@Override
+			public void onClosed() {
+				Log.d(TAG,"onClosed()");
+				handler.sendEmptyMessage(2);
+				clientIsDisconnected();
 			}
 
 			@Override
 			public void onError(String reason) {
+				Log.d(TAG,"onError() "+reason);
 				Message msg = new Message();
 				msg.what = 1;
 				Bundle data = new Bundle();
@@ -74,61 +80,61 @@ public class MainActivity extends Activity implements Handler.Callback {
 				msg.setData(data);
 				handler.sendMessage(msg);
 				
-				// let's assume something bad happened and close the webSocket
-				if (client != null) {
-					client.close();
-				}
-				connected = false;
-				buttonsDisable();
+				// let's assume something bad happened and close the connection entirely
+				connection.close();
 			}
 			
 			@Override
-			public void onDevicesEvent(List<String> deviceList) {
+			public void onDevicesEvent(String timestamp, List<String> deviceList) {
+				Log.d(TAG,"onDevicesEvent() "+deviceList.toString());
 				Message msg = new Message();
 				msg.what = 1;
 				Bundle data = new Bundle();
-				data.putString("text", "Devices: "+deviceList.toString());
+				data.putString("text", timestamp+" --- "+"Devices: "+deviceList.toString());
 				msg.setData(data);
 				handler.sendMessage(msg);
-				
 			}
 			
 			@Override
-			public void onEvent(String fromDevice, String eventId, String params) {
+			public void onEvent(String timestamp, String fromDevice, String eventId, String params) {
+				Log.d(TAG,"onEvent() "+fromDevice+" "+eventId+" "+params);
 				Message msg = new Message();
 				msg.what = 1;
 				Bundle data = new Bundle();
-				data.putString("text", "Event "+eventId+" from "+fromDevice+" with params "+params);
+				data.putString("text", timestamp+" --- "+"Event "+eventId+" from "+fromDevice+" with params "+params);
 				msg.setData(data);
 				handler.sendMessage(msg);
 			};
 			
 			@Override
-			public void onMessage(String fromDevice, String msgId, String params) {
+			public void onMessage(String timestamp, String fromDevice, String msgId, String params) {
+				Log.d(TAG,"onMessage() "+fromDevice+" "+msgId+" "+params);
 				Message msg = new Message();
 				msg.what = 1;
 				Bundle data = new Bundle();
-				data.putString("text", "Message "+msgId+" from "+fromDevice+" with params "+params);
+				data.putString("text", timestamp+" --- "+"Message "+msgId+" from "+fromDevice+" with params "+params);
 				msg.setData(data);
 				handler.sendMessage(msg);
 			}
 
 			@Override
-			public void onDeviceJoinedChannel(String device) {
+			public void onDeviceJoinedChannel(String timestamp, String device) {
+				Log.d(TAG,"onDeviceJoinedChannel() "+device);
 				Message msg = new Message();
 				msg.what = 1;
 				Bundle data = new Bundle();
-				data.putString("text", device+" joined the channel");
+				data.putString("text", timestamp+" --- "+device+" joined the channel");
 				msg.setData(data);
 				handler.sendMessage(msg);
 			}
 
 			@Override
-			public void onDeviceLeftChannel(String device) {
+			public void onDeviceLeftChannel(String timestamp, String device) {
+				Log.d(TAG,"onDeviceLeftChannel() "+device);
 				Message msg = new Message();
 				msg.what = 1;
 				Bundle data = new Bundle();
-				data.putString("text", device+" left the channel");
+				data.putString("text", timestamp+" --- "+device+" left the channel");
 				msg.setData(data);
 				handler.sendMessage(msg);
 			}
@@ -137,26 +143,26 @@ public class MainActivity extends Activity implements Handler.Callback {
     
     @Override
     protected void onDestroy() {
-    	if (client != null && client.isConnected()) {
-    		client.close();
+    	if (connection != null) {
+    		connection.close();
     	}
     	super.onDestroy();
     }
     
-    private void buttonsEnable() {
+    private void clientIsConnected() {
     	connectBtn.setText("Disconnect");
     	deviceListBtn.setEnabled(true);
     	sendEventBtn.setEnabled(true);
     }
     
-    private void buttonsDisable() {
+    private void clientIsDisconnected() {
     	connectBtn.setText("Connect");
     	deviceListBtn.setEnabled(false);
     	sendEventBtn.setEnabled(false);
     }
     
 	public void connectClick(View v) {
-	    if (connected == false) {
+	    if (connection == null || !connection.isConnectedToChannel()) {
 	        task = new AsyncTask<String,String,String>() {
 	        	ProgressDialog dialog;
 	        	
@@ -177,15 +183,11 @@ public class MainActivity extends Activity implements Handler.Callback {
 	        	
 				@Override
 				protected String doInBackground(String... params) {
-			    	client = new MewaClient(WS_URI,CHANNEL,DEVICE,PASSWORD);
-			    	client.setOnMessageListener(onMessageListener);
+					connection = new MewaConnection(WS_URI,CHANNEL,DEVICE,PASSWORD);
+					connection.setOnMessageListener(onMessageListener);
 			    	try {
-						client.connect();
+			    		connection.connect();
 					} catch (InitConnectionException e) {
-						e.printStackTrace();
-						return "error";
-					} catch (AlreadyConnectedToChannelException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 						return "error";
 					}
@@ -199,8 +201,7 @@ public class MainActivity extends Activity implements Handler.Callback {
 			        }
 			        
 			        if (result.equals("ok")) {
-				    	buttonsEnable();
-				    	connected = true;
+				    	clientIsConnected();
 			        } else {
 			        	Message msg = new Message();
 						msg.what = 1;
@@ -213,36 +214,37 @@ public class MainActivity extends Activity implements Handler.Callback {
 				
 				@Override
 				protected void onCancelled() {
-					if (client != null && client.isConnected()) {
-						client.close();
+					if (connection != null) {
+						connection.setOnMessageListener(null);
+						connection.close();
 					}
-					buttonsDisable();
-			    	connected = false;
+					clientIsDisconnected();
 				}
 	        };
 	        task.execute();
 	    } else {
-	    	client.close();
-	    	buttonsDisable();
-	    	connected = false;
+	    	connection.close();
+	    	clientIsDisconnected();
 	    }
 	}
 	
 	public void devicesListClick(View v) {
-		client.requestDevicesList();
+		connection.requestDevicesList();
 	}
 	
 	public void sendEventClick(View v) {
-		client.sendEvent("event.X", "{ }");
+		connection.sendEvent("event.X", "{ }");
 	}
 
 	@Override
 	public boolean handleMessage(Message msg) {
 		if (msg.what == 0) {
 			loggerTxt.append("Connected\n");
-		} else {
+		} else if (msg.what == 1) {
 			String text = msg.getData().getString("text");
 			loggerTxt.append(text+"\n");
+		} else {
+			loggerTxt.append("Connection closed.\n");
 		}
 		
 		return false;
